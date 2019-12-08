bcf0a8fe0350c10e7b48b8c47da6ff848d2f394f
WyerByter <wyerbyter@gmail.com>
2019-12-07 22:26:50 -0500

    Initial version of GenericDeepEqualityComparer and unit tests

---
+++ GenericDeepEquality/GenericDeepEqualityComparer.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericDeepEquality
+{
+    public class GenericDeepEqualityComparer : IEqualityComparer<object>
+    {
+        public new bool Equals(object x, object y)
+        {
+            if (x == null || y == null)
+            {
+                return x == null && y == null;
+            }
+            else if (x.GetType() != y.GetType())
+            {
+                return false;
+            }
+            else
+            {
+                switch (x)
+                {
+                    case IEnumerable<object> xEnumerable:
+                        var yEnumerable = (IEnumerable<object>)y;
+                        return EnumerableComparison(xEnumerable, yEnumerable);
+                    case bool xBool:
+                        var yBool = (bool)y;
+                        return xBool == yBool;
+                    case byte xByte:
+                        var yByte = (byte)y;
+                        return xByte == yByte;
+                    case sbyte xSByte:
+                        var ySByte = (sbyte)y;
+                        return xSByte == ySByte;
+                    case char xChar:
+                        var yChar = (char)y;
+                        return xChar == yChar;
+                    case decimal xDecimal:
+                        var yDecimal = (decimal)y;
+                        return xDecimal == yDecimal;
+                    case double xDouble:
+                        var yDouble = (double)y;
+                        return xDouble == yDouble;
+                    case float xFloat:
+                        var yFloat = (float)y;
+                        return xFloat == yFloat;
+                    case int xInt:
+                        var yInt = (int)y;
+                        return xInt == yInt;
+                    case uint xUInt:
+                        var yUInt = (uint)y;
+                        return xUInt == yUInt;
+                    case long xLong:
+                        var yLong = (long)y;
+                        return xLong == yLong;
+                    case ulong xULong:
+                        var yULong = (ulong)y;
+                        return xULong == yULong;
+                    case short xShort:
+                        var yShort = (short)y;
+                        return xShort == yShort;
+                    case ushort xUShort:
+                        var yUShort = (ushort)y;
+                        return xUShort == yUShort;
+                    case string xString:
+                        var yString = (string)y;
+                        return xString.Equals(yString);
+                    default:
+                        return GenericComparision(x, y);
+                }
+            }
+        }
+
+        public int GetHashCode(object obj)
+        {
+            return obj.GetHashCode();
+        }
+
+        private bool EnumerableComparison(
+            IEnumerable<object> x,
+            IEnumerable<object> y)
+        {
+            var equal = false;
+
+            if (x == null || y == null)
+            {
+                equal = x == null && y == null;
+            }
+            else
+            {
+                var xList = x.ToList();
+                var yList = y.ToList();
+
+                var xNotInY = xList.Any(
+                    xItem => yList.All(
+                        yItem => !Equals(
+                            yItem,
+                            xItem)));
+                var yNotInX = yList.Any(
+                    yItem => xList.All(
+                        xItem => !Equals(
+                            xItem,
+                            yItem)));
+
+                equal = xList.Count == yList.Count
+                    && (xList.Count == 0
+                        || !(xNotInY || yNotInX));
+            }
+
+            return equal;
+        }
+
+        private bool GenericComparision(object x, object y)
+        {
+            var equal = true;
+
+            if (x == null || y == null)
+            {
+                equal = x == null && y == null;
+            }
+            else
+            {
+
+                foreach (var propertyInfo in x.GetType().GetProperties())
+                {
+                    var xValue = propertyInfo.GetValue(x, null);
+                    var yValue = propertyInfo.GetValue(y, null);
+                    if (Equals(xValue, yValue))
+                        continue;
+                    equal = false;
+                    break;
+                }
+
+                if (equal)
+                {
+                    foreach (var fieldInfo in x.GetType().GetFields())
+                    {
+                        var xValue = fieldInfo.GetValue(x);
+                        var yValue = fieldInfo.GetValue(y);
+                        if (Equals(xValue, yValue))
+                            continue;
+                        equal = false;
+                        break;
+                    }
+                }
+            }
+
+            return equal;
+        }
+    }
+}
---
+++ Tests/GenericDeepEqualityTest.cs
@@ -0,0 +1,373 @@
+using GenericDeepEquality;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class GenericDeepEqualityTest
+    {
+        [Fact]
+        public void EqualsBoolTrue()
+        {
+            bool x = true;
+            bool y = true;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsBoolFalse()
+        {
+            bool x = true;
+            bool y = false;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsByteTrue()
+        {
+            byte x = 3;
+            byte y = 3;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsByteFalse()
+        {
+            byte x = 3;
+            byte y = 4;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsSByteTrue()
+        {
+            sbyte x = 3;
+            sbyte y = 3;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsSByteFalse()
+        {
+            sbyte x = 3;
+            sbyte y = 4;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsCharTrue()
+        {
+            char x = 'a';
+            char y = 'a';
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsCharFalse()
+        {
+            char x = 'a';
+            char y = 'A';
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsDecimalTrue()
+        {
+            decimal x = 3.4m;
+            decimal y = 3.4m;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsDecimalFalse()
+        {
+            decimal x = 3.4m;
+            decimal y = 4.9m;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsDoubleTrue()
+        {
+            double x = 3.2;
+            double y = 3.2;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsDoubleFalse()
+        {
+            double x = 3.2;
+            double y = 4.7;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsFloatTrue()
+        {
+            float x = 3.2f;
+            float y = 3.2f;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsFloatFalse()
+        {
+            float x = 3.2f;
+            float y = 4.7f;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsIntTrue()
+        {
+            int x = 3;
+            int y = 3;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsIntFalse()
+        {
+            int x = 3;
+            int y = 4;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsUIntTrue()
+        {
+            uint x = 3;
+            uint y = 3;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsUIntFalse()
+        {
+            uint x = 3;
+            uint y = 4;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsLongTrue()
+        {
+            long x = 3;
+            long y = 3;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsLongFalse()
+        {
+            long x = 3;
+            long y = 4;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsULongTrue()
+        {
+            ulong x = 3;
+            ulong y = 3;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsULongFalse()
+        {
+            ulong x = 3;
+            ulong y = 4;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsShortTrue()
+        {
+            short x = 3;
+            short y = 3;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsShortFalse()
+        {
+            short x = 3;
+            short y = 4;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsUShortTrue()
+        {
+            ushort x = 3;
+            ushort y = 3;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsUShortFalse()
+        {
+            ushort x = 3;
+            ushort y = 4;
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsStringTrue()
+        {
+            string x = "Right String";
+            string y = "Right String";
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsStringFalse()
+        {
+            string x = "Right String";
+            string y = "Wrong String";
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        private struct TestStruct
+        {
+            public int iNumber;
+            public string sWord;
+        }
+
+        [Fact]
+        public void EqualsStructTrue()
+        {
+            TestStruct x = new TestStruct
+            {
+                iNumber = 3,
+                sWord = "Struct Test"
+            };
+            TestStruct y = new TestStruct
+            {
+                iNumber = 3,
+                sWord = "Struct Test"
+            };
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsStructFalse()
+        {
+            TestStruct x = new TestStruct
+            {
+                iNumber = 3,
+                sWord = "Struct Test"
+            };
+            TestStruct y = new TestStruct
+            {
+                iNumber = 3,
+                sWord = "Wrong Struct Test"
+            };
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+
+        private class TestClass
+        {
+            public int iNumber;
+            public string sWord;
+        }
+
+        [Fact]
+        public void EqualsClassTrue()
+        {
+            TestClass x = new TestClass
+            {
+                iNumber = 3,
+                sWord = "Class Test"
+            };
+            TestClass y = new TestClass
+            {
+                iNumber = 3,
+                sWord = "Class Test"
+            };
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.True(comparer.Equals(x, y));
+        }
+
+        [Fact]
+        public void EqualsClassFalse()
+        {
+            TestClass x = new TestClass
+            {
+                iNumber = 3,
+                sWord = "Class Test"
+            };
+            TestClass y = new TestClass
+            {
+                iNumber = 3,
+                sWord = "Wrong Class Test"
+            };
+            var comparer = new GenericDeepEqualityComparer();
+
+            Assert.False(comparer.Equals(x, y));
+        }
+    }
+}
